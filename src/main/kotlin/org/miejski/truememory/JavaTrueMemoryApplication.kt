1213cd97d5ee0b93b329e223b9be71c60295e0cb
gmiejski <grz.miejski@gmail.com>
2017-11-30 00:57:03 +0100

    JRMEM-1 | initial commit

---
+++ src/main/kotlin/org/miejski/truememory/JavaTrueMemoryApplication.kt
@@ -0,0 +1,11 @@
+package org.miejski.truememory
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JavaTrueMemoryApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(JavaTrueMemoryApplication::class.java, *args)
+}
